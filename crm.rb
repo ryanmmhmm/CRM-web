5931f0bc7ad9f94ec6ac6e8bad43eafc2fa32d45
ryanmmhmm <magowan.ryan@gmail.com>
2015-11-05 15:26:31 -0500

    created /start route to boot app with data and return to main menu.  added feature to be able to add a contact.

--- crm.rb
+++ crm.rb
@@ -8,16 +8,25 @@ get '/' do
   erb :index, :layout => :layout
 end
 
-get '/contacts' do
+get '/start' do
   Contact.create("Ryan", "Magowan", email: "magowan.ryan@gmail.com", notes: "Creator of @@crm_app_name")
   Contact.create("Blank", "User", email: "blank_user@email.com", notes: "Blank User default")
   Contact.create("This", "Is", email: "Another@email.com", notes: "User")
+  redirect to '/'
+end
 
+get '/contacts' do
   erb :contacts  # :layout => :post
 end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+post '/contacts/new' do
+  Contact.create(params[:first_name], params[:last_name], email: params[:email], notes: params[:notes])
+
+  redirect to '/contacts'
 end
 
 get '/contacts/modify' do
